cc9d7bb40401a7f6965fdc388db8b414ba11da7f
ValkyrieKiwi <armstrong.d30320@gmail.com>
2022-06-16 16:09:01 +0100

    commit

--- complete/src/main/java/com/example/springboot/TvlController.java
+++ complete/src/main/java/com/example/springboot/TvlController.java
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.stereotype.Controller;
 //import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
